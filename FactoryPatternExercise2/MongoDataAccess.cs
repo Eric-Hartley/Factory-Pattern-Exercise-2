a03bebed03ecbc116e726df2f197c6e515ae0451
Eric Hartley <spiffydude0@gmail.com>
2024-10-10 13:50:38 -0500

    completed exercise 2

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    internal class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+                new Product() { Name = "CPU", Price = 129.99m },
+                new Product() { Name = "GPU", Price = 599.99m },
+                new Product() { Name = "Motherboard", Price = 199.99m},
+                new Product() { Name = "Keyboard", Price = 120.99m },
+                new Product() { Name = "Monitor", Price = 159.99m },
+                new Product() { Name = "Mouse", Price = 65.99m },
+                new Product() { Name = "Desk", Price = 250.99m },
+                new Product() { Name = "Power Supply", Price = 120.99m }
+        };
+
+        public List<Product> LoadData()
+        {
+            Console.WriteLine($"I am reading data from a List database.");
+
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine($"I am saving data to a Mongo database.");
+        }
+    }
+
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Get database type from the user
+            Console.WriteLine("What type of database do you want to use???");
+            Console.WriteLine("Please enter: List for a static list.");
+            Console.WriteLine("Please enter: Sql for a MySQL DB.");
+            Console.WriteLine("Please enter: Mongo for a Mongo DB.");
+            string databaseType = Console.ReadLine();
+
+            //create something (DataAccess class) that implements IDataAccess
+            IDataAccess dataAccess = DataAccessFactory.GetDataAccessType(databaseType);
+
+            //Get the list of products that LoadData returns and the run the save data method
+            List<Product> products = dataAccess.LoadData();
+            dataAccess.SaveData();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Here are your products");
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Name: {product.Name} | " +
+                    $"Price: {product.Price,0:c}");
+            }
         }
     }
 }
